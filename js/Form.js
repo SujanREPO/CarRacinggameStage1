31f9022d1d47fb4b32ba5864f44cdf4dda157880
SujanREPO <sujanmrityunjay@gmail.com>
2020-12-15 19:13:46 +0530

    addedBackimage

--- js/Form.js
+++ js/Form.js
@@ -4,7 +4,9 @@ class Form {
     this.input = createInput("Name");
     this.button = createButton('Play');
     this.greeting = createElement('h2');
-    this.title = createElement('h2');
+    this.title = createElement('h1');
+    this.reset = createButton('reset');
+
   }
   hide(){
     this.greeting.hide();
@@ -13,14 +15,24 @@ class Form {
     this.title.hide();
   }
 
+  setElementStyle(){
+    this.button.class("customButton");
+    this.reset.class("customButton");
+
+  }
+
   display(){
+    this.setElementStyle();
     this.title.html("Car Racing Game");
-    this.title.position(displayWidth/2 - 50, 0);
+    this.title.position(displayWidth/2 - 100, 0);
+
+    //this.button.html("play");
 
     this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80);
     this.button.position(displayWidth/2 + 30, displayHeight/2);
-2
-    this.button.mousePressed(()=>{
+    this.reset.position(displayWidth-350,50)
+
+     this.button.mousePressed(()=>{
       this.input.hide();
       this.button.hide();
       player.name = this.input.value();
@@ -29,8 +41,14 @@ class Form {
       player.update();
       player.updateCount(playerCount);
       this.greeting.html("Hello " + player.name)
-      this.greeting.position(displayWidth/2 - 70, displayHeight/4);
+      this.greeting.position(displayWidth/2 - 70, displayHeight/4); 
     });
-
+    this.reset.mousePressed(()=>{
+      game.update(0);
+      player.updateCount(0);
+      database.ref("players").remove();
+    }
+    
+   )
   }
 }
--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ var playerCount;
 var allPlayers;
 var distance = 0;
 var database;
+var welcomeBackground;
 
 var form, player, game;
 
@@ -13,12 +14,13 @@ var cars, car1, car2, car3, car4;
 var track, car1_img, car2_img, car3_img, car4_img;
 
 function preload(){
-  track = loadImage("../images/track.jpg");
-  car1_img = loadImage("../images/car1.png");
-  car2_img = loadImage("../images/car2.png");
-  car3_img = loadImage("../images/car3.png");
-  car4_img = loadImage("../images/car4.png");
-  ground = loadImage("../images/ground.png");
+  track = loadImage("/images/track.jpg");
+  welcomeBackground = loadImage("/images/Background.jpg");
+  car1_img = loadImage("/images/car1.png");
+  car2_img = loadImage("/images/car2.png");
+  car3_img = loadImage("/images/car3.png");
+  car4_img = loadImage("/images/car4.png");
+  ground = loadImage("images/ground.png");
 }
 
 function setup(){
@@ -31,6 +33,7 @@ function setup(){
 
 
 function draw(){
+  background(welcomeBackground);
   if(playerCount === 4){
     game.update(1);
   }
